45a0a1d330a2ecd833378ef423bd11362474c561
Robert Rothenberg <rrwo@cpan.org>
2022-09-02 13:13:08 +0100

    Perltidy Plack::Middleware::EmulateOPTIONS

--- lib/Plack/Middleware/EmulateOPTIONS.pm
+++ lib/Plack/Middleware/EmulateOPTIONS.pm
@@ -11,7 +11,7 @@ use parent 'Plack::Middleware';
 
 use Plack::Util;
 use Plack::Util::Accessor qw/ filter /;
-use HTTP::Status qw/ HTTP_OK /;
+use HTTP::Status          qw/ HTTP_OK /;
 use Try::Tiny;
 
 our $VERSION = 'v0.1.0';
@@ -21,18 +21,20 @@ sub call {
 
     my $filter = $self->filter;
 
-
     if ( $env->{REQUEST_METHOD} eq "OPTIONS" && ( !$filter || $filter->($env) ) ) {
 
-        my $res = $self->app->({ %$env, REQUEST_METHOD => "HEAD" });
+        my $res = $self->app->( { %$env, REQUEST_METHOD => "HEAD" } );
 
-        return Plack::Util::response_cb( $res, sub {
+        return Plack::Util::response_cb(
+            $res,
+            sub {
                 my ($res) = @_;
-                if ($res->[0] == HTTP_OK) {
+                if ( $res->[0] == HTTP_OK ) {
                     Plack::Util::header_set( $res->[1], 'allow', "GET, HEAD, OPTIONS" );
                 }
                 return;
-            });
+            }
+        );
 
     }
     else {
@@ -41,6 +43,5 @@ sub call {
 
     }
 }
-
 
 1;
