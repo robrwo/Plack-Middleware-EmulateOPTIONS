c636f5f81c70b131b3aae210c79ed5fba04f0184
Robert Rothenberg <rrwo@cpan.org>
2024-09-29 19:33:24 +0100

    Commit Changes and bump $VERSION

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Plack::Middleware::EmulateOPTIONS - handle OPTIONS requests as HEAD
 
 # VERSION
 
-version v0.4.0
+version v0.4.1
 
 # SYNOPSIS
 
--- lib/Plack/Middleware/EmulateOPTIONS.pm
+++ lib/Plack/Middleware/EmulateOPTIONS.pm
@@ -14,7 +14,7 @@ use HTTP::Status          ();
 
 use experimental qw( postderef signatures );
 
-our $VERSION = 'v0.4.0';
+our $VERSION = 'v0.4.1';
 
 =head1 SYNOPSIS
 
